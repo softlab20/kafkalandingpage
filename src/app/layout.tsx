64f37e74acaaf1be247ec213a7e58c9dd3f87738
OsamaHIma <osamahima018@gmail.com>
2025-08-13 12:37:13 +0300

    push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,41 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { Lalezar, Noto_Sans_Arabic } from "next/font/google";
 import "./globals.css";
 
-const lalezar = Lalezar({
-  subsets: ["latin", "arabic"],
-  weight: "400",
-  variable: "--font-lalezar",
-  display: "swap",
-});
-
-const notoSansArabic = Noto_Sans_Arabic({
-  subsets: ["latin", "arabic"],
-  weight: ["400", "600", "700", "900"],
-  variable: "--font-noto-sans-arabic",
+const dinArabic = localFont({
+  src: [
+    {
+      path: "./din-arabic/ArbFONTS-DINNextLTArabic-UltraLight-1.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./din-arabic/ArbFONTS-DINNEXTLTARABIC-LIGHT-1.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./din-arabic/ArbFONTS-DINNextLTArabic-Regular-2.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./din-arabic/ArbFONTS-DINNextLTArabic-Medium-2.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./din-arabic/ArbFONTS-DINNextLTArabic-Bold-2.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./din-arabic/ArbFONTS-DINNextLTArabic-Black-2.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-din-arabic",
   display: "swap",
 });
 
@@ -49,7 +71,7 @@ export default function RootLayout({
   return (
     <html lang="ar">
       <body
-        className={`${tufuliArabic.variable} ${notoSansArabic.variable} font-noto-sans-arabic antialiased`}
+        className={`${tufuliArabic.variable} ${dinArabic.variable} font-din-arabic antialiased`}
       >
         {children}
       </body>
